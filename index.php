96bf7f931e6b55bc99ed33256828784d01809861
szmoon <szmoon@gmail.com>
2018-02-05 17:31:42 -0800

    pull from api and print to page

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php 
-
+$data = file_get_contents('https://www.codeschool.com/users/szmoon.json');
+$json_data = json_decode($data, true);
+// var_dump($json_data['courses']['completed']);
+$courses = $json_data['courses']['completed'];
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title><?php echo $json_data['user']['username']; ?>'s badges</title>
   </head>
 
   <body>
-
+    <?php
+      foreach ($courses as $course) {
+        echo '<p>' . $course["title"] . '<p>';
+      }
+    ?>
   </body>
 </html>
